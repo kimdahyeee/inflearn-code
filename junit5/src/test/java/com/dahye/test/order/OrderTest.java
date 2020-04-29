3d40b2b107d2c54b0df8c648fd713ef26f072089
김다혜 <dahye432111@gmail.com>
2020-04-29 23:23:48 +0900

    junit-test: junit5: 테스트 인스턴스 공유, 테스트 순서

---
+++ junit5/src/test/java/com/dahye/test/order/OrderTest.java
@@ -0,0 +1,28 @@
+package com.dahye.test.order;
+
+import org.junit.jupiter.api.*;
+
+@DisplayName("테스트 순서 설정하기(TestMethodOrder), 테스트 인스턴스 공유 (TestInstance)")
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class OrderTest {
+    int value = 1;
+
+    @Order(1)
+    @Test
+    void test1() {
+        System.out.println("1 : " + value++);
+    }
+
+    @Order(3)
+    @Test
+    void test3() {
+        System.out.println("3 : " + value++);
+    }
+
+    @Order(2)
+    @Test
+    void test2() {
+        System.out.println("2 : " + value++);
+    }
+}

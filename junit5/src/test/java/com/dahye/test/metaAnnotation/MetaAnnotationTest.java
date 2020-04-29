6594352fac9aef2843b798f930d91de24ef0769c
김다혜 <dahye432111@gmail.com>
2020-04-29 22:47:48 +0900

    junit-test: junit5: 커스텀 태그
    intellij run 설정을 통해 특정 태그를 가진 테스트만 실행할 수 있다.
    
    메타 애노테이션을 만들어두면, 재사용하기 쉽고, 오타와 같은 소소한 실수를 방지할 수 있음 !

---
+++ junit5/src/test/java/com/dahye/test/metaAnnotation/MetaAnnotationTest.java
@@ -0,0 +1,17 @@
+package com.dahye.test.metaAnnotation;
+
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("메타 애노테이션 만들기")
+public class MetaAnnotationTest {
+
+    @FastTest
+    void fast() {
+        System.out.println("fast test");
+    }
+
+    @SlowTest
+    void slow() {
+        System.out.println("slow test");
+    }
+}

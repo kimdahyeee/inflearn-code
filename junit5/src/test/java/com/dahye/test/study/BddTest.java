d832909ef66377cf8989aeab2cda4eec47d5b25f
김다혜 <dahye432111@gmail.com>
2020-04-30 16:45:11 +0900

    junit-test: BDD & Mockito 연습 문제

--- junit5/src/test/java/com/dahye/test/study/BddTest.java
+++ junit5/src/test/java/com/dahye/test/study/BddTest.java
@@ -2,6 +2,7 @@ package com.dahye.test.study;
 
 import com.dahye.test.domain.Member;
 import com.dahye.test.domain.Study;
+import com.dahye.test.domain.StudyStatus;
 import com.dahye.test.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,21 +12,22 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("BDD 학습")
 public class BddTest {
+    @Mock
+    MemberService memberService;
+    @Mock
+    StudyRepository studyRepository;
+
     @Test
     @DisplayName("stubbing 과제 BDD 로 변경해보기")
-    void createNewStudy(@Mock MemberService memberService,
-                        @Mock StudyRepository studyRepository) {
+    void subbingBDD() {
         //given
         StudyService studyService = new StudyService(memberService, studyRepository);
         assertNotNull(studyService);
@@ -49,5 +51,29 @@ public class BddTest {
         assertEquals(1L, study.getOwnerId());
 
         then(memberService).should(times(1)).notify(study);
+    }
+
+    @DisplayName("다른 사용자가 볼 수 있도록 스터디를 공개한다.")
+    @Test
+    void MDDTest() {
+        // Given
+        StudyService studyService = new StudyService(memberService, studyRepository);
+        Study study = new Study(10, "더 자바, 테스트");
+        assertNull(study.getOpenedDateTime());
+        // TODO studyRepository Mock 객체의 save 메소드를호출 시 study를 리턴하도록 만들기.
+        given(studyRepository.save(study))
+                .willReturn(study);
+
+        // When
+        studyService.openStudy(study);
+
+        // Then
+        // TODO study의 status가 OPENED로 변경됐는지 확인
+        assertEquals(StudyStatus.OPENED, study.getStatus());
+        // TODO study의 openedDataTime이 null이 아닌지 확인
+        assertNotNull(study.getOpenedDateTime());
+        // TODO memberService의 notify(study)가 호출 됐는지 확인.
+        then(memberService).should().notify(study);
+
     }
 }

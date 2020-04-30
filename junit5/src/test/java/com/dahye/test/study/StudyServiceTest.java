56e4ea90f21cfd7091ebe3ec6cdb7bef25b39b09
김다혜 <dahye432111@gmail.com>
2020-04-30 16:18:56 +0900

    junit-test: Mock 객체 Stubbing 연습 문제

--- junit5/src/test/java/com/dahye/test/study/StudyServiceTest.java
+++ junit5/src/test/java/com/dahye/test/study/StudyServiceTest.java
@@ -1,6 +1,7 @@
 package com.dahye.test.study;
 
 import com.dahye.test.domain.Member;
+import com.dahye.test.domain.Study;
 import com.dahye.test.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -107,5 +108,31 @@ class StudyServiceTest {
         });
 
         assertEquals(Optional.empty(), memberService.findById(1L));
+    }
+
+    @Test
+    @DisplayName("stubbing 과제")
+    void createNewStudy(@Mock MemberService memberService,
+                        @Mock StudyRepository studyRepository) {
+        StudyService studyService = new StudyService(memberService, studyRepository);
+        assertNotNull(studyService);
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("keesun@email.com");
+
+        Study study = new Study(10, "테스트");
+
+        // TODO memberService 객체에 findById 메소드를 1L 값으로 호출하면 member 객체를 리턴하도록 Stubbing
+        when(memberService.findById(1L))
+                .thenReturn(Optional.of(member));
+        // TODO studyRepository 객체에 save 메소드를 study 객체로 호출하면 study 객체 그대로 리턴하도록 Stubbing
+        when(studyRepository.save(study))
+                .thenReturn(study);
+
+        studyService.createNewStudy(1L, study);
+
+        assertNotNull(study);
+        assertEquals(1L, study.getOwnerId());
     }
 }

344ba0babb07ab720de4e02d4686728a20a29c65
김다혜 <dahye432111@gmail.com>
2020-07-28 00:03:37 +0900

    feat: Event 생성 API 구현 - 입력값 이외에 에러 발생

--- rest-api/src/test/java/dev/inflearn/restapi/events/EventControllerTests.java
+++ rest-api/src/test/java/dev/inflearn/restapi/events/EventControllerTests.java
@@ -2,6 +2,7 @@ package dev.inflearn.restapi.events;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -27,7 +28,39 @@ public class EventControllerTests {
     ObjectMapper objectMapper;
 
     @Test
-    void createEvent() throws Exception {
+    @DisplayName("202 return")
+    void createEvent_201() throws Exception {
+        EventDto event = EventDto.builder()
+                .name("Spring")
+                .description("rest api")
+                .beginEnrollmentDateTime(LocalDateTime.of(2020, 7, 27, 23, 10, 0))
+                .closeEnrollmentDateTime(LocalDateTime.of(2020, 7, 28, 23, 10, 0))
+                .beginEventDateTime(LocalDateTime.of(2020, 7, 29, 14, 21))
+                .endEventDateTime(LocalDateTime.of(2020, 7, 30, 14, 21))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("gangnam")
+                .build();
+
+        mockMvc.perform(post("/api/events/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated()) // response 201
+                .andExpect(jsonPath("id").exists())
+                .andExpect(header().exists(HttpHeaders.LOCATION))
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, "application/hal+json"))
+                .andExpect(jsonPath("id").value(Matchers.not(100)))
+                .andExpect(jsonPath("free").value(Matchers.not(true)))
+                .andExpect(jsonPath("eventStatus").value(Matchers.not(EventStatus.DRAFT)))
+        ;
+    }
+
+    @Test
+    @DisplayName("bad request")
+    void createEvent_badRequest() throws Exception {
         Event event = Event.builder()
                 .id(100)
                 .name("Spring")
@@ -50,13 +83,7 @@ public class EventControllerTests {
                 .accept(MediaTypes.HAL_JSON)
                 .content(objectMapper.writeValueAsString(event)))
                 .andDo(print())
-                .andExpect(status().isCreated()) // response 201
-                .andExpect(jsonPath("id").exists())
-                .andExpect(header().exists(HttpHeaders.LOCATION))
-                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, "application/hal+json"))
-                .andExpect(jsonPath("id").value(Matchers.not(100)))
-                .andExpect(jsonPath("free").value(Matchers.not(true)))
-                .andExpect(jsonPath("eventStatus").value(Matchers.not(EventStatus.DRAFT)))
+                .andExpect(status().isBadRequest())
         ;
     }
 }

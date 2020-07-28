6435ee969130906c35267fb0c73082c6ba4ad497
김다혜 <dahye432111@gmail.com>
2020-07-28 22:43:37 +0900

    feat: Event 생성 API 구현: Bad Request 응답 본문 만들기
    에러 응답 메시지에 에러에 대한 정보가 있어야 한다.

--- rest-api/src/main/java/dev/inflearn/restapi/events/EventValidator.java
+++ rest-api/src/main/java/dev/inflearn/restapi/events/EventValidator.java
@@ -9,8 +9,7 @@ import java.time.LocalDateTime;
 public class EventValidator {
     public void validate(EventDto eventDto, Errors errors) {
         if (eventDto.getBasePrice() > eventDto.getMaxPrice() && eventDto.getMaxPrice() > 0) {
-            errors.rejectValue("basePrice", "wrongValue", "basePrice is Wrong");
-            errors.rejectValue("maxPrice", "wrongValue", "maxPrice is Wrong");
+            errors.reject("prices", "prices are Wrong");
         }
 
         LocalDateTime endEventDateTime = eventDto.getEndEventDateTime();
